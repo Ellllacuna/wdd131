673b920831059fef1a196bc087398108a2e6be7c
Ellllacuna <vunderbrod@gmail.com>
2025-04-30 09:26:02 -0600

    added changing logo

--- mission/index.js
+++ mission/index.js
@@ -1,4 +1,9 @@
 const themeSelector = document.getElementById("selectMode");
+const image = document.createElement("img");
+image.alt = "dark theme BYUI logo";
+image.classList.add("logo");
+document.querySelector("footer").appendChild(image);
+
 
 function changeTheme() {
     const newSelect = document.getElementById("selectMode");
@@ -7,9 +12,11 @@ function changeTheme() {
     if (selectValue === 'light') {
         document.body.classList.remove('dark');
         newSelect.classList.remove('dark');
+        image.src = 'byui-logo.webp';
     } else if (selectValue === 'dark') {
         document.body.classList.add('dark');
         newSelect.classList.add('dark');
+        image.src = "byui-logo_white.png"
     };
 };
 
